d4a9d3d913fe5ac0ceb20fc6feecf41d280c7e19
ada-doghonadze <122531948+ada-doghonadze@users.noreply.github.com>
2023-05-26 22:21:34 +0400

    add project

---
+++ main.js
@@ -0,0 +1,44 @@
+//დავალება 1
+// const btn = document.getElementById('btn');
+
+// const div = document.getElementById('div1');
+
+
+// btn.addEventListener('click', function () {
+//     div.remove();
+// })
+
+
+
+
+//დავალება2
+
+// const divEl = document.createElement('div')
+// const h2El = document.createElement('h2')
+// const textEl = document.createTextNode('Gandalf')
+// const aEl = document.createElement('a')
+// const text2El = document.createTextNode('Go to Profile')
+
+// h2El.appendChild(textEl)
+// divEl.appendChild(h2El)
+// divEl.appendChild(aEl)
+// aEl.appendChild(text2El)
+// aEl.setAttribute('href', "#")
+// console.log(divEl)
+
+
+
+//დავალება2(მეორე გზა)
+
+
+// document.body.innerHTML = `<div id="card">
+//                  <h2> Gandalf </h2>
+//                  <a href="#"> Go To Profile</a>
+//                  </div>`
+
+
+
+
+
+
+
